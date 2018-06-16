4d6a6f105962435401cc9e385e0b3029894dbf17
Arpi <arpishah2@gmail.com>
2018-06-16 13:56:31 -0700

    Singleton and prototype scope of bean

--- src/main/java/com/springing/Springing01/Client.java
+++ src/main/java/com/springing/Springing01/Client.java
@@ -1,6 +1,8 @@
 package com.springing.Springing01;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.springing.Springing01.service.BasicMessageOfTheDayImpl;
 import com.springing.Springing01.service.MessagePrinterService;
 
 public class Client {
@@ -10,11 +12,18 @@ public class Client {
 				"spring/application.xml")) {
 			context.registerShutdownHook();
 
+			final BasicMessageOfTheDayImpl basicMessageService = (BasicMessageOfTheDayImpl) context.getBean("basicMessageOfDay");
+			basicMessageService.setMessage("Hiiii");
+			System.out.println(basicMessageService.getMessage());
+			
+			final BasicMessageOfTheDayImpl basicMessageService2 = (BasicMessageOfTheDayImpl) context.getBean("basicMessageOfDay");
+			System.out.println(basicMessageService2.getMessage());
+			
 			// configuration
-			final MessagePrinterService printer = (MessagePrinterService) context.getBean("printer");
+			//final MessagePrinterService printer = (MessagePrinterService) context.getBean("printer");
 			
 			// call print message
-			printer.printMessage();
+			//printer.printMessage();
 
 		} catch (final Exception ex) {
 			ex.printStackTrace();
